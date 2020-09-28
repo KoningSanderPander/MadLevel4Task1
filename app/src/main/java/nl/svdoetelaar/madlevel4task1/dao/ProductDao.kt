3356fcb35ef5d397177e6a4e8bb24997b3de317d
Sander <sandervandenoetelaar@hotmail.com>
2020-09-28 20:31:27 +0200

    added database

--- app/src/main/java/nl/svdoetelaar/madlevel4task1/dao/ProductDao.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel4task1/dao/ProductDao.kt
@@ -1,4 +1,25 @@
 package nl.svdoetelaar.madlevel4task1.dao
 
+import androidx.navigation.NavDeepLinkDsl
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import nl.svdoetelaar.madlevel4task1.model.Product
+
+@Dao
 interface ProductDao {
+
+    @Query("SELECT * FROM product")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query ("DELETE FROM product")
+    suspend fun deleteAllProducts()
+
 }
--- app/src/main/java/nl/svdoetelaar/madlevel4task1/repository/ProductRepository.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel4task1/repository/ProductRepository.kt
@@ -1,2 +1,39 @@
 package nl.svdoetelaar.madlevel4task1.repository
 
+import android.content.Context
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import nl.svdoetelaar.madlevel4task1.dao.ProductDao
+import nl.svdoetelaar.madlevel4task1.database.ShoppingListRoomDatabase
+import nl.svdoetelaar.madlevel4task1.model.Product
+
+class ProductRepository(context: Context) {
+    private val productDao: ProductDao
+
+    init {
+        val database = ShoppingListRoomDatabase.getDatabase(context)
+        productDao = database!!.productDao()
+    }
+
+
+    suspend fun getAllProducts(): List<Product>{
+        return productDao.getAllProducts()
+    }
+
+
+    suspend fun insertProduct(product: Product) {
+        productDao.insertProduct(product)
+    }
+
+
+    suspend fun deleteProduct(product: Product){
+        productDao.deleteProduct(product)
+    }
+
+
+    suspend fun deleteAllProducts(){
+        productDao.deleteAllProducts()
+    }
+
+}
